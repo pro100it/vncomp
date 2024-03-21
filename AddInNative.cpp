cf7a4579804dbcbad9bc85401c8a7ace255d66f0
Michail <pro100it@hotmail.com>
2024-03-21 12:05:26 +0500

    Add files via upload

---
+++ AddInNative.cpp
@@ -0,0 +1,261 @@
+﻿
+#include "stdafx.h"
+
+
+#if defined( __linux__ ) || defined(__APPLE__)
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <iconv.h>
+#endif
+
+#include <wchar.h>
+#include "AddInNative.h"
+#include <string>
+
+static const WCHAR_T g_kClassNames[] = u"CAddInNative"; //|OtherClass1|OtherClass2";
+
+uint32_t convToShortWchar(WCHAR_T** Dest, const wchar_t* Source, uint32_t len = 0);
+uint32_t convFromShortWchar(wchar_t** Dest, const WCHAR_T* Source, uint32_t len = 0);
+uint32_t getLenShortWcharStr(const WCHAR_T* Source);
+static AppCapabilities g_capabilities = eAppCapabilitiesInvalid;
+static std::u16string s_names(g_kClassNames);
+//---------------------------------------------------------------------------//
+long GetClassObject(const WCHAR_T* wsName, IComponentBase** pInterface)
+{
+    if(!*pInterface)
+    {
+        *pInterface= new CAddInNative();
+        return (long)*pInterface;
+    }
+    return 0;
+}
+//---------------------------------------------------------------------------//
+AppCapabilities SetPlatformCapabilities(const AppCapabilities capabilities)
+{
+    g_capabilities = capabilities;
+    return eAppCapabilitiesLast;
+}
+//---------------------------------------------------------------------------//
+AttachType GetAttachType()
+{
+    return eCanAttachAny;
+}
+//---------------------------------------------------------------------------//
+long DestroyObject(IComponentBase** pIntf)
+{
+    if(!*pIntf)
+        return -1;
+
+    delete *pIntf;
+    *pIntf = 0;
+    return 0;
+}
+//---------------------------------------------------------------------------//
+const WCHAR_T* GetClassNames()
+{
+    return s_names.c_str();
+}
+//---------------------------------------------------------------------------//
+//CAddInNative
+CAddInNative::CAddInNative()
+{
+}
+//---------------------------------------------------------------------------//
+CAddInNative::~CAddInNative()
+{
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::Init(void* pConnection)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetInfo()
+{ 
+    return 2000; 
+}
+//---------------------------------------------------------------------------//
+void CAddInNative::Done()
+{
+}
+/////////////////////////////////////////////////////////////////////////////
+// ILanguageExtenderBase
+//---------------------------------------------------------------------------//
+bool CAddInNative::RegisterExtensionAs(WCHAR_T** wsExtensionName)
+{ 
+    return false; 
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNProps()
+{ 
+    return eLastProp;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::FindProp(const WCHAR_T* wsPropName)
+{ 
+    return -1;
+}
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::GetPropName(long lPropNum, long lPropAlias)
+{ 
+    return 0;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::GetPropVal(const long lPropNum, tVariant* pvarPropVal)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::SetPropVal(const long lPropNum, tVariant *varPropVal)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::IsPropReadable(const long lPropNum)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::IsPropWritable(const long lPropNum)
+{
+    return false;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNMethods()
+{ 
+    return eLastMethod;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::FindMethod(const WCHAR_T* wsMethodName)
+{ 
+    return -1;
+}
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::GetMethodName(const long lMethodNum, const long lMethodAlias)
+{ 
+    return 0;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNParams(const long lMethodNum)
+{ 
+    return 0;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::GetParamDefValue(const long lMethodNum, const long lParamNum,
+                        tVariant *pvarParamDefValue)
+{ 
+    return false;
+} 
+//---------------------------------------------------------------------------//
+bool CAddInNative::HasRetVal(const long lMethodNum)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::CallAsProc(const long lMethodNum,
+                    tVariant* paParams, const long lSizeArray)
+{ 
+    return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::CallAsFunc(const long lMethodNum,
+                tVariant* pvarRetValue, tVariant* paParams, const long lSizeArray)
+{ 
+    return false; 
+}
+//---------------------------------------------------------------------------//
+void CAddInNative::SetLocale(const WCHAR_T* loc)
+{
+}
+//---------------------------------------------------------------------------//
+void ADDIN_API CAddInNative::SetUserInterfaceLanguageCode(const WCHAR_T * lang)
+{
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::setMemManager(void* mem)
+{
+    return false;
+}
+//---------------------------------------------------------------------------//
+uint32_t convToShortWchar(WCHAR_T** Dest, const wchar_t* Source, size_t len)
+{
+    if (!len)
+        len = ::wcslen(Source) + 1;
+
+    if (!*Dest)
+        *Dest = new WCHAR_T[len];
+
+    WCHAR_T* tmpShort = *Dest;
+    wchar_t* tmpWChar = (wchar_t*) Source;
+    uint32_t res = 0;
+
+    ::memset(*Dest, 0, len * sizeof(WCHAR_T));
+
+#if defined( __linux__ ) || defined(__APPLE__)
+    size_t succeed = (size_t)-1;
+    size_t f = len * sizeof(wchar_t), t = len * sizeof(WCHAR_T);
+    const char* fromCode = sizeof(wchar_t) == 2 ? "UTF-16" : "UTF-32";
+    iconv_t cd = iconv_open("UTF-16LE", fromCode);
+    if (cd != (iconv_t)-1)
+    {
+        succeed = iconv(cd, (char**)&tmpWChar, &f, (char**)&tmpShort, &t);
+        iconv_close(cd);
+        if(succeed != (size_t)-1)
+            return (uint32_t)succeed;
+    }
+#endif 
+    for (; len; --len, ++res, ++tmpWChar, ++tmpShort)
+    {
+        *tmpShort = (WCHAR_T)*tmpWChar;
+    }
+
+    return res;
+}
+//---------------------------------------------------------------------------//
+uint32_t convFromShortWchar(wchar_t** Dest, const WCHAR_T* Source, uint32_t len)
+{
+    if (!len)
+        len = getLenShortWcharStr(Source) + 1;
+
+    if (!*Dest)
+        *Dest = new wchar_t[len];
+
+    wchar_t* tmpWChar = *Dest;
+    WCHAR_T* tmpShort = (WCHAR_T*)Source;
+    uint32_t res = 0;
+
+    ::memset(*Dest, 0, len * sizeof(wchar_t));
+#if defined( __linux__ ) || defined(__APPLE__)
+    size_t succeed = (size_t)-1;
+    const char* fromCode = sizeof(wchar_t) == 2 ? "UTF-16" : "UTF-32";
+    size_t f = len * sizeof(WCHAR_T), t = len * sizeof(wchar_t);
+    iconv_t cd = iconv_open("UTF-32LE", fromCode);
+    if (cd != (iconv_t)-1)
+    {
+        succeed = iconv(cd, (char**)&tmpShort, &f, (char**)&tmpWChar, &t);
+        iconv_close(cd);
+        if(succeed != (size_t)-1)
+            return (uint32_t)succeed;
+    }
+#endif 
+    for (; len; --len, ++res, ++tmpWChar, ++tmpShort)
+    {
+        *tmpWChar = (wchar_t)*tmpShort;
+    }
+
+    return res;
+}
+//---------------------------------------------------------------------------//
+uint32_t getLenShortWcharStr(const WCHAR_T* Source)
+{
+    uint32_t res = 0;
+    WCHAR_T *tmpShort = (WCHAR_T*)Source;
+
+    while (*tmpShort++)
+        ++res;
+
+    return res;
+}
+//---------------------------------------------------------------------------//
---
+++ AddInNative.h
@@ -0,0 +1,59 @@
+﻿#ifndef __ADDINNATIVE_H__
+#define __ADDINNATIVE_H__
+
+#include "ComponentBase.h"
+#include "AddInDefBase.h"
+#include "IMemoryManager.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// class CAddInNative
+class CAddInNative : public IComponentBase
+{
+public:
+    enum Props
+    {
+        eLastProp      // Always last
+    };
+
+    enum Methods
+    {
+        eLastMethod      // Always last
+    };
+
+    CAddInNative(void);
+    virtual ~CAddInNative();
+    // IInitDoneBase
+    virtual bool ADDIN_API Init(void*) override;
+    virtual bool ADDIN_API setMemManager(void* mem) override;
+    virtual long ADDIN_API GetInfo() override;
+    virtual void ADDIN_API Done() override;
+    // ILanguageExtenderBase
+    virtual bool ADDIN_API RegisterExtensionAs(WCHAR_T**) override;
+    virtual long ADDIN_API GetNProps() override;
+    virtual long ADDIN_API FindProp(const WCHAR_T* wsPropName) override;
+    virtual const WCHAR_T* ADDIN_API GetPropName(long lPropNum, long lPropAlias) override;
+    virtual bool ADDIN_API GetPropVal(const long lPropNum, tVariant* pvarPropVal) override;
+    virtual bool ADDIN_API SetPropVal(const long lPropNum, tVariant* varPropVal) override;
+    virtual bool ADDIN_API IsPropReadable(const long lPropNum) override;
+    virtual bool ADDIN_API IsPropWritable(const long lPropNum) override;
+    virtual long ADDIN_API GetNMethods() override;
+    virtual long ADDIN_API FindMethod(const WCHAR_T* wsMethodName) override;
+    virtual const WCHAR_T* ADDIN_API GetMethodName(const long lMethodNum, 
+                            const long lMethodAlias) override;
+    virtual long ADDIN_API GetNParams(const long lMethodNum) override;
+    virtual bool ADDIN_API GetParamDefValue(const long lMethodNum, const long lParamNum,
+                            tVariant *pvarParamDefValue) override;   
+    virtual bool ADDIN_API HasRetVal(const long lMethodNum) override;
+    virtual bool ADDIN_API CallAsProc(const long lMethodNum,
+                    tVariant* paParams, const long lSizeArray) override;
+    virtual bool ADDIN_API CallAsFunc(const long lMethodNum,
+                tVariant* pvarRetValue, tVariant* paParams, const long lSizeArray) override;
+    // LocaleBase
+    virtual void ADDIN_API SetLocale(const WCHAR_T* loc) override;
+    // UserLanguageBase
+    virtual void ADDIN_API SetUserInterfaceLanguageCode(const WCHAR_T* lang) override;
+    
+private:
+    // Attributes
+};
+#endif //__ADDINNATIVE_H__
